90f1e3aa71cc3db6f5032f9bc98972a62bb6bcd4
Nobert-Muma <mumanobert17@gmail.com>
2024-08-02 21:17:36 +0300

    Updated state based on events and worked with multiple state variables together

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { useState } from "react";
 
 function Item({ name, category }) {
+  const [cart, setCart]=useState(false);
+  function handleCartItems(){
+    setCart(currentState=>!currentState);
+  }
+ const liClass=cart?"in-cart":"";
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCartItems}>{cart?"Remove From Cart":"Add to Cart"}</button>
     </li>
   );
 }
